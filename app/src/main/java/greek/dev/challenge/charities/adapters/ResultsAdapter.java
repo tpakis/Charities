159b6de6df95439ee6e71ace261fb700a5f8dfe3
programbench\programbench <thanasakis@gmail.com>
2017-12-18 16:33:17 +0200

    1) comments to the model class 2) removed Sample Data used Charity model 3) changed the ResultsAdapter using charity model class 4) comments to the MainActivity class

--- app/src/main/java/greek/dev/challenge/charities/adapters/ResultsAdapter.java
+++ app/src/main/java/greek/dev/challenge/charities/adapters/ResultsAdapter.java
@@ -1,23 +1,22 @@
-package greek.dev.challenge.charities.Utilities;
+package greek.dev.challenge.charities.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import greek.dev.challenge.charities.R;
-import greek.dev.challenge.charities.SampleData;
+import greek.dev.challenge.charities.model.Charity;
 
 public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsHolder>{
 
     // using sample data until real ones are available
-    private ArrayList<SampleData> charitiesResults;
+    private ArrayList<Charity> charitiesResults;
 
     public class ResultsHolder extends RecyclerView.ViewHolder {
 
@@ -30,8 +29,9 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsH
         @BindView(R.id.charity_icon)
         public ImageView iv_charity_logo;
 
-        @BindView(R.id.rating)
-        public RatingBar rb_rating;
+        //he user can't send data to the firebase db yet, so it can't be implemented
+      //  @BindView(R.id.rating)
+       // public RatingBar rb_rating;
 
         public ResultsHolder(View view) {
             super(view);
@@ -53,11 +53,13 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsH
     @Override
     public void onBindViewHolder(ResultsHolder holder, int position) {
         //TODO implement with the true data
-        SampleData sampleDataObject = charitiesResults.get(position);
-        holder.tv_charity_name.setText(sampleDataObject.getName());
-        holder.tv_charity_info.setText(sampleDataObject.getInfo());
-        holder.iv_charity_logo.setImageResource(sampleDataObject.getLogo());
-        holder.rb_rating.setNumStars(sampleDataObject.getStars());
+        Charity charityObject = charitiesResults.get(position);
+        holder.tv_charity_name.setText(charityObject.getName());
+        holder.tv_charity_info.setText(charityObject.getDescription());
+       // has to be implemented the getIconlink provides link not image
+        // holder.iv_charity_logo.setImageResource(charityObject.getIconLink());
+        //The user can't send data to the firebase db yet, so it can't be implemented
+        //holder.rb_rating.setNumStars(sampleDataObject.getStars());
     }
 
     @Override
@@ -66,7 +68,7 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsH
         return charitiesResults.size();
     }
 
-    public void setSeriesResults(ArrayList<SampleData> charitiesResults) {
+    public void setSeriesResults(ArrayList<Charity> charitiesResults) {
         this.charitiesResults = charitiesResults;
         notifyDataSetChanged();
     }

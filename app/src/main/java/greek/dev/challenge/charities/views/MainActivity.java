5479271777df2f48b8afeead8f65d67f589ad0a4
programbench\programbench <thanasakis@gmail.com>
2017-12-21 18:32:40 +0200

    1) Ενσωματώθηκαν όλες οι αλλαγές από το master branch (Νέες activity, vector γραφικά, νέα layout)

--- app/src/main/java/greek/dev/challenge/charities/views/MainActivity.java
+++ app/src/main/java/greek/dev/challenge/charities/views/MainActivity.java
@@ -3,10 +3,15 @@ package greek.dev.challenge.charities.views;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +24,7 @@ import com.google.firebase.storage.StorageReference;
 import java.util.List;
 
 import greek.dev.challenge.charities.R;
+import greek.dev.challenge.charities.adapters.ImageAdapter;
 import greek.dev.challenge.charities.model.Charity;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,7 +39,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
 
+        gridview.setOnItemClickListener(new OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                if (position == 0) {
+                    Intent myIntent = new Intent(v.getContext(), CharitiesResultsActivity.class);
+                    startActivityForResult(myIntent, 0);
+                }
+            }
+        });
         //get reference to specific part of database - messages with mMessagesDatabaseReference
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseStorage = FirebaseStorage.getInstance();
@@ -45,9 +64,9 @@ public class MainActivity extends AppCompatActivity {
 
         viewModel.getCharitiesList().observe(MainActivity.this, new Observer<List<Charity>>() {
             @Override
-            public void onChanged(@Nullable List<Charity> itemAndPeople) {
-                Log.v("main", itemAndPeople.get(itemAndPeople.size()-1).toString());
-                Log.v("main", String.valueOf(itemAndPeople.size()));
+            public void onChanged(@Nullable List<Charity> charitiesList) {
+                Log.v("main", charitiesList.get(charitiesList.size()-1).toString());
+                Log.v("main", String.valueOf(charitiesList.size()));
             }
         });
 

06fb0a8a4bd3003b34ce9977b011c547c2fbb9b9
andreasagap <andreasagap@yahoo.gr>
2017-12-24 11:25:35 +0200

    Animation RV

--- app/src/main/java/greek/dev/challenge/charities/adapters/ResultsAdapter.java
+++ app/src/main/java/greek/dev/challenge/charities/adapters/ResultsAdapter.java
@@ -9,18 +9,23 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.mikepenz.iconics.view.IconicsImageView;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import greek.dev.challenge.charities.R;
 import greek.dev.challenge.charities.model.Charity;
 
 public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsHolder>{
 
     private ArrayList<Charity> charitiesResults;
-
+    private Map<String, Integer> map = new HashMap<String, Integer>();
     private final CharitiesResultsAdapterOnClickHandler mClickHandler;
 
     public interface CharitiesResultsAdapterOnClickHandler {
@@ -41,10 +46,10 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsH
         public TextView tv_charity_info;
 
         @BindView(R.id.rv_charity_icon)
-        public ImageView iv_charity_icon;
+        public CircleImageView iv_charity_icon;
 
         @BindView(R.id.rv_charity_default_icon)
-        public ImageView iv_charity_default_icon;
+        public IconicsImageView iv_charity_default_icon;
 
         // the user can't send data to the firebase db yet, so it can't be implemented
         // @BindView(R.id.rating)
@@ -54,6 +59,22 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsH
             super(view);
             ButterKnife.bind(this, view);
             view.setOnClickListener(this);
+            map.put("actionaid", R.drawable.actionaid);
+            map.put("antikarkiniki", R.drawable.antikarkiniki);
+            map.put("anima", R.drawable.anima);
+            map.put("edke", R.drawable.edke);
+            map.put("elepap", R.drawable.elepap);
+            map.put("elpida", R.drawable.elpida);
+            map.put("grammis_sos", R.drawable.grammis_sos);
+            map.put("i_pisti", R.drawable.i_pisti);
+            map.put("iagkalia", R.drawable.iagkalia);
+
+            map.put("kibotos", R.drawable.kibotos);
+            map.put("theofilos", R.drawable.theofilos);
+            map.put("unicef", R.drawable.unicef);
+            map.put("xamogelo_paidiou", R.drawable.xamogelo_paidiou);
+            map.put("xwria_sos", R.drawable.xwria_sos);
+
         }
 
         @Override
@@ -78,7 +99,15 @@ public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultsH
         Charity charityObject = charitiesResults.get(position);
         holder.tv_charity_name.setText(charityObject.getName());
         holder.tv_charity_info.setText(charityObject.getDescription());
+        // If the charity's icon exist
+        if(map.containsKey(charityObject.getIconlink()))
+        {
+            holder.iv_charity_icon.setImageResource(map.get(charityObject.getIconlink()));
+            holder.iv_charity_default_icon.setVisibility(View.GONE);
 
+            charitiesResults.get(position).setDrawableIconPosition(map.get(charityObject.getIconlink()));
+
+        }
          /* NOTE. if the charity's image is available, then we should use:
             holder.iv_charity_default_icon.setVisibility(View.INVISIBLE);
             holder.iv_charity_icon.setImageResource(); (or a similar method to set the source)
--- app/src/main/java/greek/dev/challenge/charities/views/CharityDetails.java
+++ app/src/main/java/greek/dev/challenge/charities/views/CharityDetails.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.mikepenz.iconics.view.IconicsImageView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 import greek.dev.challenge.charities.R;
 import greek.dev.challenge.charities.model.Charity;
 import greek.dev.challenge.charities.utilities.CallAndSms;
@@ -20,10 +24,10 @@ import greek.dev.challenge.charities.utilities.CallAndSms;
 public class CharityDetails extends AppCompatActivity {
 
     @BindView(R.id.detail_charity_icon)
-    public ImageView iv_charity_icon;
+    public CircleImageView iv_charity_icon;
 
     @BindView(R.id.detail_charity_default_icon)
-    public ImageView iv_charity_default_icon;
+    public IconicsImageView iv_charity_default_icon;
 
     @BindView(R.id.detail_charity_name)
     public TextView tv_charity_name;
@@ -87,6 +91,11 @@ public class CharityDetails extends AppCompatActivity {
 
     private void setData(Charity selectedCharity){
 
+                if(selectedCharity.getIconlink()!="")
+                {
+                    iv_charity_icon.setImageResource(selectedCharity.getDrawableIconPosition());
+                    iv_charity_default_icon.setVisibility(View.GONE);
+                }
                 tv_charity_name.setText(selectedCharity.getName());
                 tv_charity_desc.setText(selectedCharity.getDescription());
                 if (selectedCharity.getSms().equals("0")){

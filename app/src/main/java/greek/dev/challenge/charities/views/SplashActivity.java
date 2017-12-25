1d1d372485173da2ddd43e654594b29f9d7c9af8
terpal <nikos.vas@mail.com>
2017-12-25 20:52:08 +0200

    add splash screen

--- app/src/main/java/greek/dev/challenge/charities/views/SplashActivity.java
+++ app/src/main/java/greek/dev/challenge/charities/views/SplashActivity.java
@@ -4,10 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by terpal on 12/25/17.
- */
-
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {

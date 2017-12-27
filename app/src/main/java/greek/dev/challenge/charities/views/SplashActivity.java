942077d71b855392bb210168afaadaf6cc616234
terpal <nikos.vas@mail.com>
2017-12-27 18:00:06 +0200

    add onboarding screen

--- app/src/main/java/greek/dev/challenge/charities/views/SplashActivity.java
+++ app/src/main/java/greek/dev/challenge/charities/views/SplashActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private OnboardingManager onboardingManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        // Checking for first time launch
+        onboardingManager = new OnboardingManager(this);
+        if (!onboardingManager.isFirstTimeLaunch()) {
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            startActivity(new Intent(this, OnboardingActivity.class));
+        }
         finish();
     }
 }

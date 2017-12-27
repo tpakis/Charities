2fde23f479e823e62ffaa3078d68be5ab4e32cd3
nalex <nalex@aegean.gr>
2017-12-28 00:09:07 +0200

    Random Wish generator for UI testing purposes.

--- app/src/main/java/greek/dev/challenge/charities/model/Wish.java
+++ app/src/main/java/greek/dev/challenge/charities/model/Wish.java
@@ -1,6 +1,7 @@
 package greek.dev.challenge.charities.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by nalex on 26/12/2017.
@@ -27,13 +28,35 @@ public class Wish {
     public static ArrayList<Wish> createWishList (int number) {
         ArrayList wishList = new ArrayList(number);
         for (int i = 0; i < number; i ++) {
-            Wish fakeWish = new Wish("Wish" + i, "Author" + i);
-            wishList.add(i, fakeWish );
+            Random r = new Random();
+            String fakeWish = wishesArray[r.nextInt(wishesArray.length)];
+            String fakeAuthor = nameArray[r.nextInt(nameArray.length)];
+
+            Wish newWish = new Wish(fakeWish, fakeAuthor);
+            wishList.add(i, newWish );
         }
         return wishList;
 
 
 
     }
+
+    private static final String [] wishesArray = {
+            "Χρόνια πολλά",
+            "Ευτυχισμένο το 2018",
+            "Εύχομαι ό,τι καλύτερο για το ίδρυμα σας!",
+            "Μια μικρή χρηματική συμβολή με πολλή αγάπη.",
+            "Σας στέλνουμε τη δωρεά μας και τις καλύτερες ευχές μας για το νέο έτος!",
+            "Καλά μας δουλεύετε; Αυτή η εφαρμογή σκίζει," +
+                    "γιατί δε τη σκέφτηκε κανείς νωρίτερα;;; ΧΡΟΝΙΑ ΠΟΛΛΑ!"
+    };
+
+    private static final String [] nameArray = {
+            "Αριστείδης Μαρίνος",
+            "Βάσω Σ.",
+            "Παναγιώτης Παπαπαναγιώτου",
+            "Μ.Ν.",
+            "Γιάννης Χατζηχριστοδούλου"
+    };
 
 }
